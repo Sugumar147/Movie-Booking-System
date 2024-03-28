a53381c5165c2eed72b788af8010a2234567945e
Sugumar <sugumar.a2020cse@sece.ac.in>
2024-03-28 18:06:06 +0530

    2nd commit

--- src/main/java/com/example/demo/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/GlobalExceptionHandler.java
@@ -20,8 +20,7 @@ public class GlobalExceptionHandler {
 
         // Add exception class name
         modelAndView.addObject("exceptionName", ex.getClass().getName());
-
-        // Add stack trace
+        
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         ex.printStackTrace(pw);
